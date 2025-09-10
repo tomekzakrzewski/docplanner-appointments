765abc3de1d97a920658a8093e8e586dc08e35d2
tomekzakrzewski <tomekzakrzewskid@gmail.com>
2025-09-10 13:51:56 +0200

    initial commit

---
+++ db_init/001_create_appointments_tables.sql
@@ -0,0 +1,30 @@
+    -- CREATE TABLE fct_appointments (
+    --     clinic_id VARCHAR(255) NOT NULL,
+    --     appointment_date DATE NOT NULL,
+    --     appointments_count INTEGER
+    -- ) PARTITION BY RANGE (appointment_date);
+
+    -- CREATE INDEX idx_fct_appointments_clinic_id ON fct_appointments (clinic_id);
+
+
+    -- CREATE TABLE IF NOT EXISTS stg_appointments (
+    --     appointment_id BIGINT,
+    --     clinic_id VARCHAR(255),
+    --     patient_id BIGINT,
+    --     created_at TIMESTAMP
+    -- ) PARTITION BY RANGE (created_at);
+
+    CREATE TABLE IF NOT EXISTS stg_appointments (
+        appointment_id BIGINT,
+        clinic_id VARCHAR(255),
+        patient_id BIGINT,
+        created_at DATE
+    );
+
+
+    CREATE TABLE IF NOT EXISTS fct_appointments (
+        clinic_id VARCHAR(255) NOT NULL,
+        appointment_date DATE NOT NULL,
+        appointments_count INTEGER,
+        PRIMARY KEY (clinic_id, appointment_date)
+    );
